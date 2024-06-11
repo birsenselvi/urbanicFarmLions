bbd77a144e9ed6c74be3f02a73f410a0f520f20d
BirsenSelviCoskun <birsenselvi88@gmail.com>
2024-06-11 20:26:38 +0200

    Merge remote-tracking branch 'origin/main'

--- src/test/java/enums/COLOR.java
+++ src/test/java/enums/COLOR.java
@@ -10,6 +10,7 @@ public enum COLOR {
     BOX_SHADOW_BLUE("#0d6efd40","rgba(13, 110, 253, 0.25) 0px 0px 0px 4px"),
     NOTIFICATION_RADIO_BUTTON_GREEN("#7fcd57",""),
     NOTIFICATION_RADIO_BUTTON_WHITE("#fff","");
+
     private String HEX;
 
     private String RGBA;
